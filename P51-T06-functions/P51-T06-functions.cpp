d49bc5c7f54d205d31adfef4c85b1e83ca02d413
lapshun <lapshun.tatiana@gmail.com>
2025-10-16 17:22:24 +0300

    243

--- P51-T06-functions/P51-T06-functions.cpp
+++ P51-T06-functions/P51-T06-functions.cpp
@@ -11,6 +11,9 @@ int main()
     float s2 = calcArea(1.5, 2.7, 3.5);
 
     cout << "\n S1: " << s1 << "\n S2: " << s2 << "\n Sum = " << s1 + s2 << endl;
+    cout << "\n S1: " << s1 << "\n S2: " << s2 << "\n Sum = " << s1 + s2 << endl;
+    cout << "\n S1: " << s1 << "\n S2: " << s2 << "\n Sum = " << s1 + s2 << endl;
+    cout << "\n S1: " << s1 << "\n S2: " << s2 << "\n Sum = " << s1 + s2 << endl;
  
 }
 
